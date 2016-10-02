eeeadca47d9fe0037881ee19c7385248c23b2992
mrogach2350 <m.rogach@gmail.com>
2016-10-02 16:05:17 -0700

    adds new method

--- bog_app/app/controllers/creatures_controller.rb
+++ bog_app/app/controllers/creatures_controller.rb
@@ -1,5 +1,14 @@
 class CreaturesController < ApplicationController
   def index
+    @creatures = Creature.all
+  end
+
+  def new
+    @creature = Creature.new
+  end
+
+  def create
+    
   end
 
 end
